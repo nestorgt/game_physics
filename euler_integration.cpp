47283cc2cf44130970c4fda0b89e75f369f6828e
Néstor García <nestorgtrabazo@gmail.com>
2013-06-10 17:07:58 +0200

    working as intended

--- euler_integration.cpp
+++ euler_integration.cpp
@@ -6,6 +6,7 @@ int main ()
     float t = 0;
     float dt = 1;
 
+    float acceleration = 0;
     float velocity = 0;
     float position = 0;
     float force = 10;
@@ -13,10 +14,16 @@ int main ()
     
     while ( t <= 10 )
     {
+        // acceleration = force divided by mass
+        acceleration = force / mass;
+        // change in position = velocity times delta time
         position = position + velocity * dt;
-        velocity = velocity + ( force / mass ) * dt;
+        // change in velocity = acceleration times delta time
+        velocity = velocity + acceleration * dt;
         t = t + dt;
+        
         printf("velocity=%2.f, position=%2.f, t=%2.f, dt=%2.f \n",velocity,position,t,dt);
     }
-        return 0;
+    
+    return 0;
 }
